c568a8fcbc6cdbe4c52a4090ee891c92e1645aed
Moaath Ibrahim <no7asweb@gmail.com>
2025-06-01 13:02:34 +0300

    sleep

--- lib/auth.ts
+++ lib/auth.ts
@@ -15,10 +15,4 @@ export const auth = betterAuth({
     plugins: [
         nextCookies()
     ],
-    socialProviders: { 
-        github: { 
-           clientId: process.env.GITHUB_CLIENT_ID as string, 
-           clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
-        }, 
-    }, 
 });
